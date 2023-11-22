a4d5e50b9834eadf189367942d5642d5a1ccf36d
itarek99 <tarekulislam7060@gmail.com>
2023-11-22 17:53:15 +0600

    connceted to mongodb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const handleFetch = async () => {
+    const res = await fetch("http://localhost:4000/", {
+      method: "GET",
+      credentials: "include",
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <>
       <div>
@@ -17,19 +24,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <button onClick={handleFetch}>Fetch</button>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,39 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
-const port = 4000;
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// app.use(
+//   session({
+//     secret: "asdfasdf",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === "production",
+//     },
+//   })
+// );
+
+mongoose
+  .connect(process.env.LOCAL_MONGO_URL || "")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.cookie("test", "test", { httpOnly: true, secure: process.env.NODE_ENV === "production" });
+  res.json({ message: "Hello World" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
